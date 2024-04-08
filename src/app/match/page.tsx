56a781c57c04c8389add65c6ce5bc5942b0006a8
Andrew <trolevan26299@gmail.com>
2024-04-08 11:34:37 +0700

    update code

--- src/app/match/page.tsx
+++ src/app/match/page.tsx
@@ -4,9 +4,9 @@ import { useTelegram } from "@/context/telegram.provider";
 import { HomeView } from "@/sections/home/view";
 import { useEffect } from "react";
 
-export const metadata = {
-  title: "Home",
-};
+// export const metadata = {
+//   title: "Home",
+// };
 export default function Home() {
   const telegram = useTelegram();
   useEffect(() => {
