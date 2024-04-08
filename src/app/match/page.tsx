9e6acf9d869757c1bc78e45275c0d13c13fdf25e
Andrew <trolevan26299@gmail.com>
2024-04-08 11:31:52 +0700

    update code

--- src/app/match/page.tsx
+++ src/app/match/page.tsx
@@ -1,8 +1,16 @@
+import { useTelegram } from "@/context/telegram.provider";
 import { HomeView } from "@/sections/home/view";
+import { useEffect } from "react";
 
 export const metadata = {
   title: "Home",
 };
 export default function Home() {
+  const telegram = useTelegram();
+  useEffect(() => {
+    if (telegram.webApp) {
+      telegram.webApp.headerColor = "rgba(18,32,46,1)";
+    }
+  }, []);
   return <HomeView />;
 }
