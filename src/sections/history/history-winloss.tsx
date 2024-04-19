4486175b1bc345a3df9b23f940fdfcf5c92ce7a5
unknown <trolevan26299@gmai.com>
2024-04-19 14:23:22 +0700

    update code

--- src/sections/history/history-winloss.tsx
+++ src/sections/history/history-winloss.tsx
@@ -69,19 +69,19 @@ const HistoryWinLoss = () => {
   const totalCommission = historyWinLose.reduce((total, item) => total + item.customerCommission, 0);
   const totalWinLoss = historyWinLose.reduce((total, item) => total + (item?.winLoss || 0), 0);
 
-  // useEffect(() => {
-  //   if (telegram?.user?.id) {
-  //     fetchBetHistory(telegram?.user?.id);
-  //     telegram.webApp?.expand();
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [telegram?.user?.id]);
   useEffect(() => {
-    fetchBetHistory(6359530967);
-    telegram.webApp?.expand();
-
+    if (telegram?.user?.id) {
+      fetchBetHistory(telegram?.user?.id);
+      telegram.webApp?.expand();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [date]);
+  }, [telegram?.user?.id]);
+  // useEffect(() => {
+  //   fetchBetHistory(6359530967);
+  //   telegram.webApp?.expand();
+
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [date]);
 
   console.log("date", date);
   const setRange = (days: number) => {
