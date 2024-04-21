191ab4f80227abb712819ec89c990eee342be269
unknown <trolevan26299@gmai.com>
2024-04-21 16:40:44 +0700

    update code

--- src/app/api/odds/route.ts
+++ src/app/api/odds/route.ts
@@ -1,12 +1,13 @@
+import { axiosInstance, endpoints } from "@/utils/axios";
 import axios from "axios";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request, res: NextApiResponse) {
   const body = await req.json();
   console.log("============>body<===================", body);
   try {
-    const response = await axios.post("https://5648-103-119-154-221.ngrok-free.app/search/match", body);
+    const response = await axiosInstance.post(endpoints.match, body);
     return NextResponse.json(response.data);
   } catch (error: any) {
     return NextResponse.json({ message: error.message });
