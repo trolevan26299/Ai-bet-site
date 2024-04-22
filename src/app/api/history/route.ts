e6e1cec135da39733d7e0e88b6dd8f2b607d2c9c
unknown <trolevan26299@gmai.com>
2024-04-22 11:01:16 +0700

    update code

--- src/app/api/history/route.ts
+++ src/app/api/history/route.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request, res: NextApiResponse) {
   const body = await req.json();
   try {
-    const url_full = "abc123123213";
+    const url_full = process.env.HOST_API_P88 + body.url;
     const response = await axiosInstance.post(endpoints.history, { ...body, url: url_full });
     console.log("body:", body);
     console.log("endpoints.history:", endpoints.history);
