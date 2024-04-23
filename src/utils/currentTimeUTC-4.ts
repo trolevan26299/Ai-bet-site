84c82ab39749ac9096f946d3a4c9e783510199a5
unknown <trolevan26299@gmai.com>
2024-04-23 11:04:45 +0700

    update code

--- src/utils/currentTimeUTC-4.ts
+++ src/utils/currentTimeUTC-4.ts
@@ -1,6 +1,6 @@
 import moment from "moment-timezone";
 
 export const currentTimeUtcMinus4 = () => {
-  const currentTime = moment().tz("America/New_York").tz("EDT");
+  const currentTime = moment().tz("America/New_York");
   return currentTime.format("YYYY-MM-DD HH:mm:ss");
 };
