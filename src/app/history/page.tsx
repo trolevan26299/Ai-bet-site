f265fe7d76358381b1619df39072db21dfd30c57
unknown <trolevan26299@gmai.com>
2024-04-24 15:22:24 +0700

    update code

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { useTelegram } from "@/context/telegram.provider";
 import { HistoryView } from "@/sections/history/view";
-import { useEffect } from "react";
 
 export default function History() {
   return <HistoryView />;
