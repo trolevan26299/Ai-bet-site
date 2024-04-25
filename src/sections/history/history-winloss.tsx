a2a8b7cd0e4124fa7cf5f3da03f8e50c4f2e53dd
unknown <trolevan26299@gmai.com>
2024-04-25 13:04:26 +0700

    update code

--- src/sections/history/history-winloss.tsx
+++ src/sections/history/history-winloss.tsx
@@ -132,19 +132,19 @@ const HistoryWinLoss = () => {
     setSelectTime(true);
   };
 
-  // useEffect(() => {
-  //   if (telegram?.user?.id) {
-  //     fetchBetHistory(telegram?.user?.id);
-  //     telegram.webApp?.expand();
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [telegram?.user?.id, date]);
   useEffect(() => {
-    fetchBetHistory(6359530967);
-    telegram.webApp?.expand();
-
+    if (telegram?.user?.id) {
+      fetchBetHistory(telegram?.user?.id);
+      telegram.webApp?.expand();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [date]);
+  }, [telegram?.user?.id, date]);
+  // useEffect(() => {
+  //   fetchBetHistory(6359530967);
+  //   telegram.webApp?.expand();
+
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [date]);
 
   return (
     <>
