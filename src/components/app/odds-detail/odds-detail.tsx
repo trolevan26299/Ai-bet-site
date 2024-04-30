3b3a04402e105653146e6669ff1c4d2a97686466
unknown <trolevan26299@gmai.com>
2024-04-30 11:48:35 +0700

    update code

--- src/components/app/odds-detail/odds-detail.tsx
+++ src/components/app/odds-detail/odds-detail.tsx
@@ -55,11 +55,7 @@ export default function OddsDetail({
   return (
     <>
       <Tabs defaultValue="1" className="w-full">
-        <TabsList
-          className={`w-full gap-3 justify-between ${
-            isSticky ? "fixed top-0 bg-backgroundColor-main rounded-none z-30" : ""
-          }`}
-        >
+        <TabsList className={`w-full gap-3 justify-between`}>
           <TabsTrigger value="1">Tất cả kèo</TabsTrigger>
           <TabsTrigger value="2">Kèo cược chấp</TabsTrigger>
           <TabsTrigger value="3">Kèo tài xỉu </TabsTrigger>
