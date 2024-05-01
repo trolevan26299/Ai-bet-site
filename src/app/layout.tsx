603e70c8b983c8b462b3185571a91ac81ad9d64c
unknown <trolevan26299@gmai.com>
2024-05-01 16:59:05 +0700

    update code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
       <body className={inter.className}>
         <TelegramProvider>
           <Theme>
