4025f5a76184bb511ee4625fffb8be713130ac01
unknown <trolevan26299@gmai.com>
2024-05-01 16:23:07 +0700

    update code

--- src/components/app/odds-detail/odds-detail.tsx
+++ src/components/app/odds-detail/odds-detail.tsx
@@ -20,16 +20,6 @@ import { m } from "framer-motion";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../../ui/accordion";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Label } from "@radix-ui/react-select";
 
 export default function OddsDetail({
   odds,
@@ -46,47 +36,49 @@ export default function OddsDetail({
   };
 
   return (
-    <Tabs defaultValue="1" className="w-full h-full">
-      <TabsList className={`w-full gap-3 justify-between`}>
-        <TabsTrigger value="1">Tất cả kèo</TabsTrigger>
-        <TabsTrigger value="2">Kèo cược chấp</TabsTrigger>
-        <TabsTrigger value="3">Kèo tài xỉu </TabsTrigger>
-      </TabsList>
+    <>
+      <Tabs defaultValue="1" className="w-full">
+        <TabsList className={`w-full gap-3 justify-between`}>
+          <TabsTrigger value="1">Tất cả kèo</TabsTrigger>
+          <TabsTrigger value="2">Kèo cược chấp</TabsTrigger>
+          <TabsTrigger value="3">Kèo tài xỉu </TabsTrigger>
+        </TabsList>
 
-      <TabsContent value="1">
-        <RenderAccordion
-          odds={odds}
-          openItems={openItems}
-          oddsStatus={oddsStatus}
-          onValueChange={handleValueChange}
-          dataScreenInfo={dataScreenInfo[0]}
-        />
-      </TabsContent>
+        <TabsContent value="1">
+          <RenderAccordion
+            odds={odds}
+            openItems={openItems}
+            oddsStatus={oddsStatus}
+            onValueChange={handleValueChange}
+            dataScreenInfo={dataScreenInfo[0]}
+          />
+        </TabsContent>
 
-      <TabsContent value="2">
-        <RenderAccordion
-          odds={odds.filter(
-            (item) => item.name_Odds === "Kèo cược chấp - Toàn trận" || item.name_Odds === "Kèo cược chấp - Hiệp 1"
-          )}
-          openItems={openItems}
-          oddsStatus={oddsStatus}
-          onValueChange={handleValueChange}
-          dataScreenInfo={dataScreenInfo[0]}
-        />
-      </TabsContent>
+        <TabsContent value="2">
+          <RenderAccordion
+            odds={odds.filter(
+              (item) => item.name_Odds === "Kèo cược chấp - Toàn trận" || item.name_Odds === "Kèo cược chấp - Hiệp 1"
+            )}
+            openItems={openItems}
+            oddsStatus={oddsStatus}
+            onValueChange={handleValueChange}
+            dataScreenInfo={dataScreenInfo[0]}
+          />
+        </TabsContent>
 
-      <TabsContent value="3">
-        <RenderAccordion
-          odds={odds.filter(
-            (item) => item.name_Odds === "Kèo tài xỉu - Toàn trận" || item.name_Odds === "Kèo tài xỉu - Hiệp 1"
-          )}
-          openItems={openItems}
-          oddsStatus={oddsStatus}
-          onValueChange={handleValueChange}
-          dataScreenInfo={dataScreenInfo[0]}
-        />
-      </TabsContent>
-    </Tabs>
+        <TabsContent value="3">
+          <RenderAccordion
+            odds={odds.filter(
+              (item) => item.name_Odds === "Kèo tài xỉu - Toàn trận" || item.name_Odds === "Kèo tài xỉu - Hiệp 1"
+            )}
+            openItems={openItems}
+            oddsStatus={oddsStatus}
+            onValueChange={handleValueChange}
+            dataScreenInfo={dataScreenInfo[0]}
+          />
+        </TabsContent>
+      </Tabs>
+    </>
   );
 }
 
@@ -186,99 +178,74 @@ function RenderAccordion({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [oddsStatus]);
 
-  useEffect(() => {
-    document.documentElement.style.scrollBehavior = "auto";
-    return () => {
-      document.documentElement.style.scrollBehavior = "smooth";
-    };
-  }, []);
-
   return (
-    // <Drawer
-    //   onClose={() => {
-    //     setSelectedTeam(null);
-    //     setValueSelectNew(undefined);
-    //   }}
-    // >
-    <Dialog>
+    <Drawer
+      onClose={() => {
+        setSelectedTeam(null);
+        setValueSelectNew(undefined);
+      }}
+    >
       <Accordion type="multiple" value={openItems} onValueChange={onValueChange} className="w-full">
-        <>
-          {odds.map((oddsGroup: IOddsDetail, index: number) => (
-            <AccordionItem value={`item-${index + 1}`} key={index}>
-              <AccordionTrigger className="text-base">{oddsGroup?.name_Odds}</AccordionTrigger>
-              <AccordionContent>
-                <DialogTrigger asChild>
-                  <div className="grid grid-cols-2 gap-[6px]">
-                    {oddsGroup?.detail?.map((match: any, matchIndex: number) => {
-                      return match?.map((team: IOdds, teamIndex: number) => {
-                        const statusKey = `${index}-${matchIndex}-${teamIndex}`;
-                        return (
-                          <div
-                            className="text-primary-foreground p-2 h-10 text-xs relative bg-[#28374a] rounded-[10px]"
-                            key={teamIndex}
-                            onClick={() => handleSelectTeam(statusKey, team, oddsGroup.name_Odds)}
-                          >
-                            {animationState.showBlink && (
-                              <>
-                                <m.div
-                                  className="absolute rotate-[45deg] right-0 top-[2px] transform translate-y-1/2 w-0 h-0 border-l-6 border-l-transparent border-r-6 border-r-transparent border-b-[6px] border-b-green-500"
-                                  style={{ display: oddsStatus[statusKey] === "green" ? "block" : "none" }}
-                                  animate={{ opacity: [0, 1, 0], rotate: [35] }}
-                                  transition={{ duration: 0.5, repeat: Infinity }}
-                                ></m.div>
+        {odds.map((oddsGroup: IOddsDetail, index: number) => (
+          <AccordionItem value={`item-${index + 1}`} key={index}>
+            <AccordionTrigger className="text-base">{oddsGroup?.name_Odds}</AccordionTrigger>
+            <AccordionContent>
+              <DrawerTrigger asChild>
+                <div className="grid grid-cols-2 gap-[6px]">
+                  {oddsGroup?.detail?.map((match: any, matchIndex: number) => {
+                    return match?.map((team: IOdds, teamIndex: number) => {
+                      const statusKey = `${index}-${matchIndex}-${teamIndex}`;
+                      return (
+                        <div
+                          className="text-primary-foreground p-2 h-10 text-xs relative bg-[#28374a] rounded-[10px]"
+                          key={teamIndex}
+                          onClick={() => handleSelectTeam(statusKey, team, oddsGroup.name_Odds)}
+                        >
+                          {animationState.showBlink && (
+                            <>
+                              <m.div
+                                className="absolute rotate-[45deg] right-0 top-[2px] transform translate-y-1/2 w-0 h-0 border-l-6 border-l-transparent border-r-6 border-r-transparent border-b-[6px] border-b-green-500"
+                                style={{ display: oddsStatus[statusKey] === "green" ? "block" : "none" }}
+                                animate={{ opacity: [0, 1, 0], rotate: [35] }}
+                                transition={{ duration: 0.5, repeat: Infinity }}
+                              ></m.div>
 
-                                <m.div
-                                  className="absolute rotate-[-45deg] right-0 bottom-1 transform translate-y-1/2 w-0 h-0 border-l-6 border-l-transparent border-r-6 border-r-transparent border-t-[6px] border-t-red-500"
-                                  style={{ display: oddsStatus[statusKey] === "red" ? "block" : "none" }}
-                                  animate={{ opacity: [0, 1, 0], rotate: [-45] }}
-                                  transition={{ duration: 0.5, repeat: Infinity }}
-                                ></m.div>
-                              </>
-                            )}
+                              <m.div
+                                className="absolute rotate-[-45deg] right-0 bottom-1 transform translate-y-1/2 w-0 h-0 border-l-6 border-l-transparent border-r-6 border-r-transparent border-t-[6px] border-t-red-500"
+                                style={{ display: oddsStatus[statusKey] === "red" ? "block" : "none" }}
+                                animate={{ opacity: [0, 1, 0], rotate: [-45] }}
+                                transition={{ duration: 0.5, repeat: Infinity }}
+                              ></m.div>
+                            </>
+                          )}
 
-                            <div className="grid grid-cols-12 w-full h-full items-center">
-                              <div
-                                style={{
-                                  maskImage: "linear-gradient(90deg, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0) 100%)",
-                                }}
-                                className="col-span-9 text-gray-300  text-sm font-medium text-text-noActive overflow-hidden whitespace-nowrap"
-                              >
-                                {`(${team.rate_odds})`} {team.name}
-                              </div>
-                              <div className=" col-span-3 text-end flex items-center justify-end text-sm font-medium text-text-red ">
-                                {team.value && (
-                                  <span
-                                    className={`${team.value >= 0 ? "text-text-green w-12" : "text-text-red w-12"} `}
-                                  >
-                                    {team.value}
-                                  </span>
-                                )}
-                              </div>
+                          <div className="grid grid-cols-12 w-full h-full items-center">
+                            <div
+                              style={{
+                                maskImage: "linear-gradient(90deg, rgba(0, 0, 0, 1) 80%, rgba(0, 0, 0, 0) 100%)",
+                              }}
+                              className="col-span-9 text-gray-300  text-sm font-medium text-text-noActive overflow-hidden whitespace-nowrap"
+                            >
+                              {`(${team.rate_odds})`} {team.name}
+                            </div>
+                            <div className=" col-span-3 text-end flex items-center justify-end text-sm font-medium text-text-red ">
+                              {team.value && (
+                                <span className={`${team.value >= 0 ? "text-text-green w-12" : "text-text-red w-12"} `}>
+                                  {team.value}
+                                </span>
+                              )}
                             </div>
                           </div>
-                        );
-                      });
-                    })}
-                  </div>
-                </DialogTrigger>
-              </AccordionContent>
-            </AccordionItem>
-          ))}
-        </>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>Make changes to your profile here. Click save when youre done.</DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">fsdfsdfsdfsdfsdf</div>
-            <div className="grid grid-cols-4 items-center gap-4">fsfsdfsdf</div>
-          </div>
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-        {/* <DrawerContent className="bg-backgroundColor-main  w-full ">
+                        </div>
+                      );
+                    });
+                  })}
+                </div>
+              </DrawerTrigger>
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+        <DrawerContent className="bg-backgroundColor-main  w-full">
           <DrawerHeader>
             <DrawerTitle className="text-[20px] text-left text-text-light">Thông tin kèo đã chọn</DrawerTitle>
           </DrawerHeader>
@@ -360,9 +327,8 @@ function RenderAccordion({
               </Button>
             </DrawerClose>
           </DrawerFooter>
-        </DrawerContent> */}
+        </DrawerContent>
       </Accordion>
-    </Dialog>
-    // </Drawer>
+    </Drawer>
   );
 }
--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -32,7 +32,8 @@ const DrawerContent = React.forwardRef<
     <DrawerOverlay />
     <DrawerPrimitive.Content
       ref={ref}
-      className={cn("fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-3xl  bg-background", className)}
+      className={cn("fixed inset-x-0 top-0 z-50 flex h-auto flex-col rounded-b-3xl bg-background", className)}
+      style={{ marginTop: "100px", height: "calc(100% - 100px)" }} // Thiết lập khoảng cách từ trên xuống và chiều cao
       {...props}
     >
       <div className="mx-auto mt-4 h-1 w-[100px] rounded-full bg-muted" />
