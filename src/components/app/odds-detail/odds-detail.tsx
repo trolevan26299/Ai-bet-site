31ae4f259e2e346bd0cc48138217efd29fd7857b
unknown <trolevan26299@gmai.com>
2024-05-01 16:13:52 +0700

    update code

--- src/components/app/odds-detail/odds-detail.tsx
+++ src/components/app/odds-detail/odds-detail.tsx
@@ -270,7 +270,7 @@ function RenderAccordion({
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
             <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>Make changes to your profile here. Click save when you're done.</DialogDescription>
+            <DialogDescription>Make changes to your profile here. Click save when youre done.</DialogDescription>
           </DialogHeader>
           <div className="grid gap-4 py-4">
             <div className="grid grid-cols-4 items-center gap-4">fsdfsdfsdfsdfsdf</div>
