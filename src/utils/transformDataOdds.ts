2d990cbe5fbfc4cfd533f6d60c6076a0eed2e314
unknown <trolevan26299@gmai.com>
2024-05-01 10:22:23 +0700

    update code

--- src/utils/transformDataOdds.ts
+++ src/utils/transformDataOdds.ts
@@ -57,111 +57,108 @@ export const transformData = (data: IMatchData[]) => {
           )
           .slice(0, 3);
 
-      const result = [];
-
-      if (spreadsToanTran && spreadsToanTran.length > 0) {
-        result.push({
+      return [
+        {
           name_Odds: "Kèo cược chấp - Toàn trận",
-          detail: spreadsToanTran.map((spread: IBetDetail) => [
-            {
-              name: item.home,
-              rate_odds: spread.hdp,
-              value: spread.home,
-              game_orientation: item.home,
-              eventId: spread.eventId,
-              lineId: spread.lineId,
-              altLineId: spread.altLineId,
-            },
-            {
-              name: item.away,
-              rate_odds: spread.hdp === 0 ? 0 : spread.hdp ?? 0 >= 0 ? -(spread.hdp ?? 0) : Math.abs(spread.hdp ?? 0),
-              value: spread.away,
-              game_orientation: item.away,
-              eventId: spread.eventId,
-              lineId: spread.lineId,
-              altLineId: spread.altLineId,
-            },
-          ]),
-        });
-      }
-      if (spreadsHiep1 && spreadsHiep1.length > 0) {
-        result.push({
+          detail:
+            spreadsToanTran &&
+            spreadsToanTran?.map((spread: IBetDetail) => [
+              {
+                name: item.home,
+                rate_odds: spread.hdp,
+                value: spread.home,
+                game_orientation: item.home,
+                eventId: spread.eventId,
+                lineId: spread.lineId,
+                altLineId: spread.altLineId,
+              },
+              {
+                name: item.away,
+                rate_odds: spread.hdp === 0 ? 0 : spread.hdp ?? 0 >= 0 ? -(spread.hdp ?? 0) : Math.abs(spread.hdp ?? 0),
+                value: spread.away,
+                game_orientation: item.away,
+                eventId: spread.eventId,
+                lineId: spread.lineId,
+                altLineId: spread.altLineId,
+              },
+            ]),
+        },
+        {
           name_Odds: "Kèo cược chấp - Hiệp 1",
-          detail: spreadsHiep1.map((spread: IBetDetail) => [
-            {
-              name: item.home,
-              rate_odds: spread.hdp,
-              value: spread.home,
-              game_orientation: item.home,
-              eventId: spread.eventId,
-              lineId: spread.lineId,
-              altLineId: spread.altLineId,
-            },
-            {
-              name: item.away,
-              rate_odds: spread.hdp === 0 ? 0 : spread.hdp ?? 0 >= 0 ? -(spread.hdp ?? 0) : Math.abs(spread.hdp ?? 0),
-              value: spread.away,
-              game_orientation: item.away,
-              eventId: spread.eventId,
-              lineId: spread.lineId,
-              altLineId: spread.altLineId,
-            },
-          ]),
-        });
-      }
-
-      if (totalTaiXiuToanTran && totalTaiXiuToanTran.length > 0) {
-        result.push({
+          detail:
+            spreadsHiep1 &&
+            spreadsHiep1?.map((spread: IBetDetail) => [
+              {
+                name: item.home,
+                rate_odds: spread.hdp,
+                value: spread.home,
+                game_orientation: item.home,
+                eventId: spread.eventId,
+                lineId: spread.lineId,
+                altLineId: spread.altLineId,
+              },
+              {
+                name: item.away,
+                rate_odds: spread.hdp === 0 ? 0 : spread.hdp ?? 0 >= 0 ? -(spread.hdp ?? 0) : Math.abs(spread.hdp ?? 0),
+                value: spread.away,
+                game_orientation: item.away,
+                eventId: spread.eventId,
+                lineId: spread.lineId,
+                altLineId: spread.altLineId,
+              },
+            ]),
+        },
+        {
           name_Odds: "Kèo tài xỉu - Toàn trận",
-          detail: totalTaiXiuToanTran.map((total: IBetDetail) => [
-            {
-              name: "Tài",
-              rate_odds: total.points,
-              value: total.over,
-              game_orientation: "over",
-              eventId: total.eventId,
-              lineId: total.lineId,
-              altLineId: total.altLineId,
-            },
-            {
-              name: "Xỉu",
-              rate_odds: total.points,
-              value: total.under,
-              game_orientation: "under",
-              eventId: total.eventId,
-              lineId: total.lineId,
-              altLineId: total.altLineId,
-            },
-          ]),
-        });
-      }
-
-      if (totalTaiXiuHiep1 && totalTaiXiuHiep1.length > 0) {
-        result.push({
+          detail:
+            totalTaiXiuToanTran &&
+            totalTaiXiuToanTran.map((total: IBetDetail) => [
+              {
+                name: "Tài",
+                rate_odds: total.points,
+                value: total.over,
+                game_orientation: "over",
+                eventId: total.eventId,
+                lineId: total.lineId,
+                altLineId: total.altLineId,
+              },
+              {
+                name: "Xỉu",
+                rate_odds: total.points,
+                value: total.under,
+                game_orientation: "under",
+                eventId: total.eventId,
+                lineId: total.lineId,
+                altLineId: total.altLineId,
+              },
+            ]),
+        },
+        {
           name_Odds: "Kèo tài xỉu - Hiệp 1",
-          detail: totalTaiXiuHiep1.map((total: IBetDetail) => [
-            {
-              name: "Tài",
-              rate_odds: total.points,
-              value: total.over,
-              game_orientation: "over",
-              eventId: total.eventId,
-              lineId: total.lineId,
-              altLineId: total.altLineId,
-            },
-            {
-              name: "Xỉu",
-              rate_odds: total.points,
-              value: total.under,
-              game_orientation: "under",
-              eventId: total.eventId,
-              lineId: total.lineId,
-              altLineId: total.altLineId,
-            },
-          ]),
-        });
-      }
-      return result;
+          detail:
+            totalTaiXiuHiep1 &&
+            totalTaiXiuHiep1.map((total: IBetDetail) => [
+              {
+                name: "Tài",
+                rate_odds: total.points,
+                value: total.over,
+                game_orientation: "over",
+                eventId: total.eventId,
+                lineId: total.lineId,
+                altLineId: total.altLineId,
+              },
+              {
+                name: "Xỉu",
+                rate_odds: total.points,
+                value: total.under,
+                game_orientation: "under",
+                eventId: total.eventId,
+                lineId: total.lineId,
+                altLineId: total.altLineId,
+              },
+            ]),
+        },
+      ];
     })
     .flat();
 };
