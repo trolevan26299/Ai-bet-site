ad58c9c8d5ff730bf3f3db177252189b71c1f0ce
unknown <trolevan26299@gmai.com>
2024-05-09 17:07:06 +0700

    update code

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -48,7 +48,7 @@ const DialogContent = React.forwardRef<HTMLDivElement, HTMLMotionProps<"div">>(
       >
         {children as ReactNode}
         <DialogClose className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-          <X className="h-4 w-4" />
+          <X className="h-6 w-6 text-white" /> {/* Thay đổi kích thước và màu sắc của biểu tượng "X" */}
           <span className="sr-only">Close</span>
         </DialogClose>
       </motion.div>
