bd036d8ec9091394e3fa074e05043d85e88e1f8c
unknown <trolevan26299@gmai.com>
2024-05-09 16:20:32 +0700

    update code

--- src/hook/useLockScroll.ts
+++ src/hook/useLockScroll.ts
@@ -4,8 +4,9 @@ export const UseLockBodyScroll = (lock: boolean) => {
   const originalStyle = window.getComputedStyle(document.body).overflow; // Get the current overflow style
   if (lock) {
     return (document.body.style.overflow = "hidden");
+  } else {
+    return () => {
+      document.body.style.overflow = originalStyle; // Restore the original overflow style
+    };
   }
-  return () => {
-    document.body.style.overflow = originalStyle; // Restore the original overflow style
-  };
 };
