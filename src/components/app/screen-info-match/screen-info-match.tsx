6c1b817744ddcf3d405bd08a8184bb9d8d53e6c9
unknown <trolevan26299@gmai.com>
2024-06-02 10:07:52 +0700

    update code

--- src/components/app/screen-info-match/screen-info-match.tsx
+++ src/components/app/screen-info-match/screen-info-match.tsx
@@ -9,7 +9,6 @@ import TeamLogo from "../../cloudinary/teamlogo";
 import { formatLiveScope } from "@/utils/formatLiveScope";
 
 export default function ScreenInfoMatch({ dataScreenInfo }: { dataScreenInfo: IMatchData[] }) {
-  console.log("122132131231");
   return (
     <div className="relative pb-2 min-h-[200px]">
       <img src="/assets/bg_team.jpg" alt="Stadium" className="w-full h-auto" />
@@ -33,12 +32,7 @@ export default function ScreenInfoMatch({ dataScreenInfo }: { dataScreenInfo: IM
             </div>
             <div className="flex justify-center items-center pt-3">
               <div className="flex flex-row justify-center gap-1 items-center w-[40px] text-white font-bold mr-2">
-                {dataScreenInfo[0]?.homeRedCards && dataScreenInfo[0].homeRedCards !== 0 && (
-                  <>
-                    {/* <p>{dataScreenInfo[0]?.homeRedCards}</p> */}
-                    <div className="bg-red-600 w-2 h-3" />
-                  </>
-                )}
+                231312313
               </div>
               <div
                 style={{ backgroundColor: "rgba(255,255,255,0.1)", border: "1px solid rgba(255,255,255,0.4)" }}
@@ -53,7 +47,7 @@ export default function ScreenInfoMatch({ dataScreenInfo }: { dataScreenInfo: IM
               >
                 <p className="font-[600] text-[20px] text-[#fafafa] ">{dataScreenInfo[0].awayScore || 0}</p>
               </div>
-              <div className="flex flex-row justify-center gap-1 items-center w-[35px] ml-2 text-white font-bold">
+              <div className="flex flex-row justify-center gap-1 items-center w-[40px] ml-2 text-white font-bold">
                 {dataScreenInfo[0]?.awayRedCards && dataScreenInfo[0].awayRedCards !== 0 && (
                   <>
                     <div className="bg-red-600 w-2 h-3" />
