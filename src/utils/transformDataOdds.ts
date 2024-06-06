fc25978cfa1b85481a603dc12ecb457e722b645a
unknown <trolevan26299@gmai.com>
2024-06-06 14:00:31 +0700

    update code

--- src/utils/transformDataOdds.ts
+++ src/utils/transformDataOdds.ts
@@ -40,7 +40,8 @@ export const transformData = (data: IMatchData[], line: string) => {
       const spreadsHiep1 = filterBets(item.bets.spreads, keoChinhHiep1, Number(line), "spreads");
       const totalTaiXiuToanTran = filterBets(item.bets.totals, keoChinhTaiXiuToanTran, Number(line), "totals");
       const totalTaiXiuHiep1 = filterBets(item.bets.totals, keoChinhTaiXiuHiep1, Number(line), "totals");
-
+      console.log("spreadsToanTran", spreadsToanTran);
+      console.log("spreadsHiep1", spreadsHiep1);
       const result = [];
 
       if (spreadsToanTran && spreadsToanTran.length > 0) {
