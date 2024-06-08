77c9d7490641d04b65e9b0db425f4e5b7da2fc01
unknown <trolevan26299@gmai.com>
2024-06-08 10:12:23 +0700

    update code

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -1,7 +1,6 @@
 export const formatNumber = (num: number) => {
   if (num || num === 0) {
     if (Math.floor(num) !== num) {
-      // Check if it's not an integer
       return num.toFixed(2).replace(/\.?0+$/, "");
     }
     return num.toString();
