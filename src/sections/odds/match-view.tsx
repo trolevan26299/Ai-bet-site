1775b05013ecb40402bea4ec9378ce489cec77b8
unknown <trolevan26299@gmai.com>
2024-06-30 14:42:18 +0700

    update code

--- src/sections/odds/match-view.tsx
+++ src/sections/odds/match-view.tsx
@@ -164,7 +164,7 @@ export default function MatchView() {
               style={{
                 border: 0,
                 width: "100%",
-                height: iframeLoaded ? (isMobileDevice() ? "354px" : "410px") : "0px",
+                height: iframeLoaded ? (isMobileDevice() ? "410px" : "354px") : "0px",
                 borderRadius: "5px",
               }}
             ></iframe>
