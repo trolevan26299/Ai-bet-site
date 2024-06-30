c59cf1c27739beddaab9de4368d46e243d327196
unknown <trolevan26299@gmai.com>
2024-06-30 14:11:57 +0700

    update code

--- src/sections/odds/match-view.tsx
+++ src/sections/odds/match-view.tsx
@@ -25,7 +25,6 @@ export default function MatchView() {
   const [loading, setLoading] = useState(true);
   const [endBet, setEndBet] = useState(false);
   const telegram = useTelegram();
-  const iframeRef = useRef<HTMLIFrameElement>(null);
 
   const matchParam = searchParams.get("match");
   const lineParam = searchParams.get("line");
@@ -130,14 +129,11 @@ export default function MatchView() {
   }, [latestOdds, endBet]);
 
   useEffect(() => {
-    if (iframeRef.current) {
-      iframeRef.current.src =
-        "https://start26.sptpub.com/tracker.html?eventId=45843693&sportId=1&lang=vi&liveEvent=true&providers=Betradar";
-      iframeRef.current.addEventListener("load", handleIframeLoad);
-    }
-    return () => {
-      iframeRef.current?.removeEventListener("load", handleIframeLoad);
-    };
+    const timer = setTimeout(() => {
+      setIframeLoaded(true);
+    }, 3000); // 3 seconds delay
+
+    return () => clearTimeout(timer);
   }, []);
   return (
     <MainLayout>
@@ -156,18 +152,18 @@ export default function MatchView() {
         <>
           <div className="p-3 pb-6 h-full">
             <ScreenInfoMatch dataScreenInfo={dataScreenInfo} />
-            {/* {iframeRef.current && ( */}
-            <div className="w-full">
-              <iframe
-                ref={iframeRef}
-                scrolling="no"
-                allowFullScreen
-                title="rindle"
-                style={{ border: 0, width: "100%", height: "354px", borderRadius: "5px" }}
-              ></iframe>
-            </div>
-            {/* )} */}
-
+            {iframeLoaded && (
+              <div className="w-full">
+                <iframe
+                  scrolling="no"
+                  src="https://start26.sptpub.com/tracker.html?eventId=45843693&sportId=1&lang=vi&liveEvent=true&providers=Betradar"
+                  allowFullScreen
+                  onLoad={handleIframeLoad}
+                  title="rindle"
+                  style={{ border: 0, width: "100%", height: "354px", borderRadius: "5px" }}
+                ></iframe>
+              </div>
+            )}
             <OddsDetail odds={odds} oddsStatus={oddsStatus} dataScreenInfo={dataScreenInfo} />
           </div>
           {odds.every((odd) => odd.status === 2) && (
