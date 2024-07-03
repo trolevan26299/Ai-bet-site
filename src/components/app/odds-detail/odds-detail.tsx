cf7a5b9db40b079eece48160269cc8f311313b06
unknown <trolevan26299@gmai.com>
2024-07-03 16:01:01 +0700

    update code

--- src/components/app/odds-detail/odds-detail.tsx
+++ src/components/app/odds-detail/odds-detail.tsx
@@ -215,7 +215,7 @@ function RenderAccordion({
       <Dialog open={showErrorConfirm}>
         <DialogContent className="sm:max-w-md text-center rounded-sm w-[80%] bg-backgroundColor-main text-text-main">
           <DialogHeader>
-            <DialogDescription className="pt-5">
+            <DialogDescription className="py-4 text-text-main text-[15px]">
               Hiện có giao dịch khác đang được xử lý. Vui lòng thử lại sau giây lát!
             </DialogDescription>
           </DialogHeader>
