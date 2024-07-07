e30e2e6b3255f8fdf4efd6b3b7376ac3d4330fb9
unknown <trolevan26299@gmai.com>
2024-07-07 14:50:54 +0700

    update code

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,7 +1,8 @@
-"use client";
-
 import { HistoryView } from "@/sections/history/view";
 
+export const metadata = {
+  title: "Winning - OutStanding Screen",
+};
 export default function History() {
   return <HistoryView />;
 }
--- src/app/match/page.tsx
+++ src/app/match/page.tsx
@@ -1,12 +1,8 @@
 import { MatchView } from "@/sections/odds";
 
+export const metadata = {
+  title: "Betting Screen",
+};
 export default function Home() {
-  // const telegram = useTelegram();
-  // useEffect(() => {
-  //   if (telegram.webApp) {
-  //     telegram.webApp.headerColor = "rgba(18,32,46,1)";
-  //   }
-  // }, []);
-
   return <MatchView />;
 }
