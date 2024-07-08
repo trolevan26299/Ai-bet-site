ec40da5fe572fb2ba16634bbc31dca4c29b31e8c
unknown <trolevan26299@gmai.com>
2024-07-08 10:47:33 +0700

    update code

--- src/components/app/odds-detail/odds-detail.tsx
+++ src/components/app/odds-detail/odds-detail.tsx
@@ -309,7 +309,7 @@ function RenderAccordion({
                                   isDisabled || disableOdds ? "text-[rgba(74,86,100,1)]" : "text-[rgba(157,163,177,1)]"
                                 } overflow-hidden whitespace-nowrap`}
                               >
-                                {team.rate_odds && `(${team.rate_odds})`} {team.name}
+                                {team.rate_odds !== undefined && `(${team.rate_odds})`} {team.name}
                               </div>
                               <div className=" col-span-3 text-end flex items-center justify-end text-sm font-medium text-text-red ">
                                 {team.value && (
@@ -514,7 +514,7 @@ function RenderAccordion({
                                   isDisabled || disableOdds ? "text-[rgba(74,86,100,1)]" : "text-[rgba(157,163,177,1)]"
                                 } overflow-hidden whitespace-nowrap`}
                               >
-                                {team.rate_odds && `(${team.rate_odds})`} {team.name}
+                                {team.rate_odds !== undefined && `(${team.rate_odds})`} {team.name}
                               </div>
                               <div className=" col-span-3 text-end flex items-center justify-end text-sm font-medium text-text-red ">
                                 {team.value && (
