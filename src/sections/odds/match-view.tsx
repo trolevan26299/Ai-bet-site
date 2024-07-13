f01766e4154060cd683b8191997e39090221ac76
unknown <trolevan26299@gmai.com>
2024-07-13 08:23:57 +0700

    update code

--- src/sections/odds/match-view.tsx
+++ src/sections/odds/match-view.tsx
@@ -14,7 +14,6 @@ import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { isIOS } from "react-device-detect";
-import { serializeThemeParams } from "@telegram-apps/sdk";
 
 export default function MatchView() {
   const searchParams = useSearchParams();
@@ -33,21 +32,6 @@ export default function MatchView() {
   const telegram = useTelegram();
   const [closingBehavior] = initClosingBehavior();
   const [swipeBehavior] = initSwipeBehavior();
-  serializeThemeParams({
-    accentTextColor: "#6ab2f2",
-    bgColor: "#17212b",
-    buttonColor: "#5288c1",
-    buttonTextColor: "#ffffff",
-    destructiveTextColor: "#ec3942",
-    headerBgColor: "#17212b",
-    hintColor: "#708499",
-    linkColor: "#6ab3f3",
-    secondaryBgColor: "#232e3c",
-    sectionBgColor: "#17212b",
-    sectionHeaderTextColor: "#6ab3f3",
-    subtitleTextColor: "#708499",
-    textColor: "#f5f5f5",
-  });
   const matchParam = searchParams.get("match");
   const lineParam = searchParams.get("line");
   const tracker_id = searchParams.get("tracker_id");
