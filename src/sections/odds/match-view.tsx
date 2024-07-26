6761111f43274dbbea4620613167c6663de3c731
unknown <trolevan26299@gmai.com>
2024-07-26 11:04:14 +0700

    update code

--- src/sections/odds/match-view.tsx
+++ src/sections/odds/match-view.tsx
@@ -277,7 +277,7 @@ export default function MatchView() {
 
   return (
     <MainLayout>
-      <div style={{ paddingBottom: "40px" }}>
+      <div style={{ paddingBottom: "50px" }}>
         {loading ? (
           <SplashScreen />
         ) : (
