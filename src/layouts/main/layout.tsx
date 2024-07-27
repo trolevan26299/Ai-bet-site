f78e5b1daf92bdfb8077a6ee9f8d3fa631cd4703
unknown <trolevan26299@gmai.com>
2024-07-27 15:44:51 +0700

    update code

--- src/layouts/main/layout.tsx
+++ src/layouts/main/layout.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 export default function MainLayout({ children }: Props) {
   return (
-    <div className="flex flex-col bg-backgroundColor-main h-full min-h-screen font-sans">
+    <div className="flex flex-col bg-[rgba(12,17,25,1)] h-full min-h-screen font-sans">
       <main className="flex-grow">{children}</main>
     </div>
   );
