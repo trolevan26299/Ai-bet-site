df524a3984969795776233afcba385df9626a49c
unknown <trolevan26299@gmai.com>
2024-07-29 09:18:43 +0700

    update code

--- src/sections/odds/match-view.tsx
+++ src/sections/odds/match-view.tsx
@@ -460,25 +460,10 @@ export default function MatchView() {
               <TabsTrigger value="password">Password</TabsTrigger>
             </TabsList>
             <TabsContent value="account">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Account</CardTitle>
-                  <CardDescription>Make changes to your account here. Click save when done.</CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                  <div className="space-y-1">
-                    <Label htmlFor="name">Name</Label>
-                    <Input id="name" defaultValue="Pedro Duarte" />
-                  </div>
-                  <div className="space-y-1">
-                    <Label htmlFor="username">Username</Label>
-                    <Input id="username" defaultValue="@peduarte" />
-                  </div>
-                </CardContent>
-                <CardFooter>
-                  <Button>Save changes</Button>
-                </CardFooter>
-              </Card>
+              <p>nội dung thẻ account</p>
+            </TabsContent>
+            <TabsContent value="password">
+              <p>nội dung thẻ password</p>
             </TabsContent>
           </Tabs>
         </PopoverContent>
