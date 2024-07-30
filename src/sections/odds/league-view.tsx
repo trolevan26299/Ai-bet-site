1a2b4d425e9143d87778aa0dec3ba14b72ebdc73
unknown <trolevan26299@gmai.com>
2024-07-30 14:06:59 +0700

    update code

--- src/sections/odds/league-view.tsx
+++ src/sections/odds/league-view.tsx
@@ -3,9 +3,26 @@ import { useRouter } from "next/navigation";
 
 const LeagueView = () => {
   const router = useRouter();
+  const handleNavigate = () => {
+    const matchId = "1594429137";
+    const queryParams = {
+      request_id: "5c785988-a377-450d-8c79-4196857d28f2",
+      match: "Kuching FA,Penang FC", // Sử dụng chuỗi thay vì mảng
+      time: "today",
+      league: "Malaysia - President Cup U20",
+      line: "0",
+      from_date: "",
+      to_date: "",
+      match_id: "1594429137",
+      tracker_id: "",
+    };
+    const queryString = new URLSearchParams(queryParams).toString();
+    router.push(`/match/${matchId}?${queryString}`);
+  };
+
   return (
     <div>
-      <button onClick={() => router.push("/match/2132132131313")}>Đến màn hình chi tiết</button>
+      <button onClick={handleNavigate}>Đến màn hình chi tiết</button>
     </div>
   );
 };
