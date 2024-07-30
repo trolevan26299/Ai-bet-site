e85e1a0f29144016b2c05976e3de441b69747736
unknown <trolevan26299@gmai.com>
2024-07-30 13:34:41 +0700

    update code

--- src/sections/odds/league-view.tsx
+++ src/sections/odds/league-view.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const LeagueView = () => {
-  return <div>league-view</div>;
+  const router = useRouter();
+  return (
+    <div>
+      <button onClick={() => router.push("/match/2132132131313")}>Đến màn hình chi tiết</button>
+    </div>
+  );
 };
 
 export default LeagueView;
