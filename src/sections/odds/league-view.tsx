c203356df3113eff7d6c98147cdb76e475d8fe81
unknown <trolevan26299@gmai.com>
2024-07-31 16:31:08 +0700

    update code

--- src/sections/odds/league-view.tsx
+++ src/sections/odds/league-view.tsx
@@ -243,7 +243,7 @@ const LeagueView = () => {
         </div>
         <div className="pt-2 w-full">
           <div
-            className="flex flex-row w-full justify-center items-center gap-2 overflow-x-auto whitespace-nowrap no-scrollbar py-1 px-2"
+            className="flex flex-row w-full justify-center items-center gap-2 overflow-x-auto whitespace-nowrap no-scrollbar py-1 px-2 hover:cursor-pointer"
             ref={league1ListRef}
           >
             {/* <div className="flex flex-row items-center justify-center gap-1 rounded-[20px] bg-[rgba(41,53,67,1)] h-8 px-[18px]">
