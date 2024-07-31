ba58ca1e53ce3a7800b0e1e38ea266191912e182
unknown <trolevan26299@gmai.com>
2024-07-31 14:25:59 +0700

    update code

--- src/sections/odds/league-view.tsx
+++ src/sections/odds/league-view.tsx
@@ -99,96 +99,102 @@ const LeagueView = () => {
   }, []);
   return (
     <MainLayout>
-      <div>
-        <div className="search h-12 flex flex-row w-full justify-center items-center bg-[rgba(17,17,17,1)]">
-          <div className="w-[18%] pl-2 flex flex-row justify-center">
-            <button className="w-[53px] h-[21px] bg-[rgba(255,255,255,1)] text-[rgba(230,58,58,1)] uppercase rounded-[16.83px] text-[12px] font-bold flex flex-row justify-center items-center">
-              Live
-              <Icon icon="pepicons-pop:circle" width={17} height={17} className="text-[rgba(230,58,58,1)]" />
-            </button>
-          </div>
+      <Popover>
+        <div>
+          <div className="search h-12 flex flex-row w-full justify-center items-center bg-[rgba(17,17,17,1)]">
+            <div className="w-[18%] pl-2 flex flex-row justify-center">
+              <button className="w-[53px] h-[21px] bg-[rgba(255,255,255,1)] text-[rgba(230,58,58,1)] uppercase rounded-[16.83px] text-[12px] font-bold flex flex-row justify-center items-center">
+                Live
+                <Icon icon="pepicons-pop:circle" width={17} height={17} className="text-[rgba(230,58,58,1)]" />
+              </button>
+            </div>
 
-          <div
-            className="w-[62%] flex flex-row justify-center gap-4 items-center  overflow-x-auto whitespace-nowrap no-scrollbar"
-            ref={tabsListRef}
-          >
-            {demoDateSearch.map((date, index) => (
-              <div
-                className="hover:cursor-pointer hover:text-[rgba(255,255,255,1)] flex flex-col items-center justify-center text-[12px] font-bold gap-[2px] text-[rgba(109,109,109,1)]"
-                key={index}
-              >
-                <p>{getDayOfWeek(date)}</p>
-                <p>{getDayAndMonth(date)}</p>
-              </div>
-            ))}
-          </div>
-
-          <div className="w-[18%] flex flex-row justify-around items-center ">
-            <Popover open={openCalendar} onOpenChange={() => setOpenSearch(!openCalendar)}>
-              {/* <PopoverTrigger asChild> */}
-              <Icon
-                icon="bx:calendar"
-                width={25}
-                height={25}
-                className="hover:cursor-pointer text-[rgba(255,255,255,1)]"
-                onClick={() => setOpenCalendar(!openCalendar)}
-              />
-              {/* </PopoverTrigger> */}
-              <PopoverContent
-                className="py-2 w-[95%] m-auto bg-[rgba(40,55,74,1)] mt-4 max-h-[80vh] rounded-sm"
-                align="center"
-              >
-                <Input
-                  type="search"
-                  placeholder="Tìm kiếm"
-                  className=" rounded-sm text-[rgba(255,255,255,1)] px-2 pb-[8px] border-none pl-2"
-                />
+            <div
+              className="w-[62%] flex flex-row justify-center gap-4 items-center  overflow-x-auto whitespace-nowrap no-scrollbar"
+              ref={tabsListRef}
+            >
+              {demoDateSearch.map((date, index) => (
                 <div
-                  className="flex flex-row justify-start items-center  gap-2 py-1 mt-2 pl-2"
-                  style={{ borderTop: "1px solid rgba(255,255,255,1)", borderBottom: "1px solid rgba(255,255,255,1)" }}
+                  className="hover:cursor-pointer hover:text-[rgba(255,255,255,1)] flex flex-col items-center justify-center text-[12px] font-bold gap-[2px] text-[rgba(109,109,109,1)]"
+                  key={index}
                 >
-                  <Button variant="secondary" color="" size="sm">
-                    Đội
-                  </Button>
-                  <Button variant="secondary" size="sm">
-                    Giải đấu
-                  </Button>
+                  <p>{getDayOfWeek(date)}</p>
+                  <p>{getDayAndMonth(date)}</p>
                 </div>
+              ))}
+            </div>
 
-                <div className="justify-start items-center pt-2 pl-2 text-white overflow-y-auto">
-                  {frameworks.map((framework, index) => (
-                    <div className="hover:bg-[rgba(255,255,255,1)] hover:text-[#000] hover:cursor-pointer" key={index}>
-                      <p className="pl-2">{framework.label}</p>
-                    </div>
-                  ))}
-                </div>
-              </PopoverContent>
-            </Popover>
-            <Popover open={openSearch} onOpenChange={() => setOpenCalendar(!openSearch)}>
-              {/* <PopoverTrigger asChild> */}
-              <Icon
-                icon="ic:baseline-search"
-                width={25}
-                height={25}
-                className="hover:cursor-pointer text-[rgba(255,255,255,1)]"
-                onClick={() => setOpenSearch(!openSearch)}
-              />
-              {/* </PopoverTrigger> */}
-              <PopoverContent
-                className="py-2 w-[95%] m-auto bg-[rgba(40,55,74,1)] mt-4 max-h-[80vh] rounded-sm"
-                align="center"
-              >
-                <div className="p-2 text-white">
-                  <p>Calendar content goes here...</p>
-                </div>
-              </PopoverContent>
-            </Popover>
+            <div className="w-[18%] flex flex-row justify-around items-center ">
+              <PopoverTrigger asChild>
+                <Icon
+                  icon="bx:calendar"
+                  width={25}
+                  height={25}
+                  className="hover:cursor-pointer text-[rgba(255,255,255,1)]"
+                  onClick={() => setOpenCalendar(!openCalendar)}
+                />
+              </PopoverTrigger>
+              <PopoverTrigger asChild>
+                <Icon
+                  icon="ic:baseline-search"
+                  width={25}
+                  height={25}
+                  className="hover:cursor-pointer text-[rgba(255,255,255,1)]"
+                  onClick={() => setOpenSearch(!openSearch)}
+                />
+              </PopoverTrigger>
+            </div>
           </div>
+          <div className="league">đây là các thẻ giải</div>
+          <div className="detail">nội dung </div>
+          <Menu />
         </div>
-        <div className="league">đây là các thẻ giải</div>
-        <div className="detail">nội dung </div>
-        <Menu />
-      </div>
+        {openSearch ? (
+          <PopoverContent
+            className="py-2 w-[95%] m-auto bg-[rgba(40,55,74,1)] mt-4 max-h-[80vh] rounded-sm"
+            align="center"
+          >
+            <Input
+              type="search"
+              placeholder="Tìm kiếm"
+              className=" rounded-sm text-[rgba(255,255,255,1)] px-2 pb-[8px] border-none pl-2"
+            />
+            <div
+              className="flex flex-row justify-start items-center  gap-2 py-1 mt-2 pl-2"
+              style={{ borderTop: "1px solid rgba(255,255,255,1)", borderBottom: "1px solid rgba(255,255,255,1)" }}
+            >
+              <Button variant="secondary" color="" size="sm">
+                Đội
+              </Button>
+              <Button variant="secondary" size="sm">
+                Giải đấu
+              </Button>
+            </div>
+
+            <div className="justify-start items-center pt-2 pl-2 text-white overflow-y-auto">
+              {frameworks.map((framework, index) => (
+                <div className="hover:bg-[rgba(255,255,255,1)] hover:text-[#000] hover:cursor-pointer" key={index}>
+                  <p className="pl-2">{framework.label}</p>
+                </div>
+              ))}
+            </div>
+          </PopoverContent>
+        ) : (
+          ""
+        )}
+        {openCalendar ? (
+          <PopoverContent
+            className="py-2 w-[95%] m-auto bg-[rgba(40,55,74,1)] mt-4 max-h-[80vh] rounded-sm"
+            align="center"
+          >
+            <div className="p-2 text-white">
+              <p>Calendar content goes here...</p>
+            </div>
+          </PopoverContent>
+        ) : (
+          ""
+        )}
+      </Popover>
     </MainLayout>
   );
 };
