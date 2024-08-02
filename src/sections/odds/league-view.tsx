c13631def81df61bf37da1b54d927ea0fae551df
unknown <trolevan26299@gmai.com>
2024-08-02 10:05:36 +0700

    update code

--- src/sections/odds/league-view.tsx
+++ src/sections/odds/league-view.tsx
@@ -265,9 +265,9 @@ const LeagueView = () => {
             className="flex flex-row w-full justify-start items-center gap-2 overflow-x-auto whitespace-nowrap no-scrollbar px-2 hover:cursor-pointer"
             ref={league1ListRef}
           >
-            <div className="flex flex-row items-center justify-center gap-1 rounded-[20px] bg-[rgba(41,53,67,1)] h-8 px-[18px]">
+            <div className="flex flex-row items-center justify-center gap-1 rounded-[20px] bg-[rgba(41,53,67,1)] h-8 px-[10px]">
               <p className=" font-bold text-[rgba(255,255,255,1)] text-sm">Tất cả</p>
-              <Badge variant="default">151</Badge>
+              <Badge variant="secondary">151</Badge>
               <Icon icon="bx:chevron-down" width={20} height={20} className="text-[rgba(255,255,255,1)]" />
             </div>
             {tagDemo.slice(0, 4).map((tag, index) => (
