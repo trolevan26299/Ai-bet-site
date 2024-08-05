6d9f2f49aa3e6d5eeada159a436e076ed4ca1803
unknown <trolevan26299@gmai.com>
2024-08-05 09:51:50 +0700

    update code

--- src/sections/odds/match-view-detail.tsx
+++ src/sections/odds/match-view-detail.tsx
@@ -406,11 +406,11 @@ export default function MatchViewDetail() {
             className="hover:cursor-pointer text-[rgba(143,149,156,1)] hover:text-[rgba(255,255,255,1)]"
             onClick={() => router.back()}
           />
-          <div className="text-[rgba(255,255,255,1)] flex flex-row justify-center items-center pr-[10px]">
+          <div className="text-[rgba(255,255,255,1)] flex flex-row justify-center items-center ">
             <Image src="/assets/league_logo.png" alt="no-content" className="w-[34px] h-[27.2px]" />
             <Popover.Root onOpenChange={(open) => handleOpenLeague(open)}>
               <Popover.Trigger>
-                <div className="flex flex-row justify-center items-center hover:cursor-pointer">
+                <div className="flex flex-row justify-center items-center hover:cursor-pointer w-[80%]">
                   <p className="text-sm font-bold w-[85%] leading-[1.1rem]">{dataScreenInfo[0]?.league_name}</p>
                   <Icon
                     icon="icon-park-solid:down-one"
