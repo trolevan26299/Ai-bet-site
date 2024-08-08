93edbcd4d48b651c0b2177b7e21f9129db1139da
unknown <trolevan26299@gmai.com>
2024-08-08 15:31:34 +0700

    update code

--- src/sections/history/history-outstanding.tsx
+++ src/sections/history/history-outstanding.tsx
@@ -88,7 +88,7 @@ const HistoryOutstanding = () => {
                 <p className="pt-4 text-base text-slate-500 font-semibold">Không có vé cược nào</p>
               </div>
             ) : (
-              <div className="mt-[95px] pb-3">
+              <div className="mt-[100px] pb-3 mb-[55px]">
                 {historyOutStanding.map((item: any) => {
                   return <HistoryItem key={item.betId} dataDetail={item} type="outstanding" />;
                 })}
